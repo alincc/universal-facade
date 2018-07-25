9ce94ad6bb83e657ccc7e60b9c2451facbe8f655
William Welling <wwelling@outlook.com>
2018-07-25 16:47:08 -0500

    upgrade angular, minor cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,6 @@ import { Store } from '@ngrx/store';
 
 import { AppState } from './core/store';
 import { LoadScaffoldAction } from './core/store/scaffold/scaffold.actions';
-import { selectScaffoldByName } from './core/store/scaffold';
-import { Observable } from 'rxjs';
-import { Scaffold } from './core/model/scaffold';
 
 @Component({
     selector: 'facade-root',
@@ -14,16 +11,12 @@ import { Scaffold } from './core/model/scaffold';
 })
 export class AppComponent implements OnInit {
 
-    public scaffold: Observable<Scaffold>;
-
     constructor(private store: Store<AppState>) {
 
     }
 
     ngOnInit() {
         this.store.dispatch(new LoadScaffoldAction());
-
-        this.scaffold = this.store.select(selectScaffoldByName('LoginRequest'));
     }
 
 }
